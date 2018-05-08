b31026ee82b9c0e27dab95341518f2683ad2fa2c
Terence Mangram <tmangram@gmail.com>
2018-05-07 20:29:10 -0500

    add bodyparser

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+
 const app = express();
+// BODYPARSER CONFIG
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // DB CONFIG
 const db = require("./config/keys").mongoURI;
@@ -12,7 +19,11 @@ mongoose.connect(db)
 	.then(() => console.log("MongoDB Connected"))
 	.catch(err => console.log("Error", err));
 
-app.get("/", (req, res) => res.send("Hello there"));
+// PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+
+// PASSPORT CONFIG
+require("./config/passport")(passport);
 
 // USE ROUTES
 app.use("/api/users", users);
