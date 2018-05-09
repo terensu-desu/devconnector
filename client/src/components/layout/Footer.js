52b8d40bcc80aefe953d14839798f506ba0eb42e
Terence Mangram <tmangram@gmail.com>
2018-05-08 23:24:59 -0500

    Initial commit

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const footer = props => (
+	<footer className="bg-dark text-white mt-5 p-5 text-center">
+		Copyright &copy; {new Date().getFullYear()} DevConnector
+	</footer>
+);
+
+export default footer;
